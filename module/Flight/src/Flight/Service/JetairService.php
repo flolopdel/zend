16f8c3a2cf5de69ea5406cac366f42b59f970e49
System Administrator <root@MacBook-Pro.local>
2016-10-03 02:29:55 +0200

    llamadas al api correctas

--- module/Flight/src/Flight/Service/JetairService.php
+++ module/Flight/src/Flight/Service/JetairService.php
@@ -10,16 +10,48 @@ use Zend\Http\Request;
      /**
       * {@inheritDoc}
       */
+
      protected $user;
      protected $password;
+
+     /*
+     * MANDATORYPARAMS 
+     * 
+     * {('locale' => 'nl_BE'),('departureairport' => 'OST'),('destinationairport' => 'ALC'), ('departuredate' => '20160401'), ('adults' => '1'), ('children' => '1'), ('infants' => '1')}
+     *
+     */
+     protected $mandatoryParams;
 
      public function __construct()
      {
         $this->user = "sollicit";
         $this->password = "ac0pX97n";
+        $this->mandatoryParams = array(
+            'locale' => 'nl_BE',
+            'departureairport' => 'ORY',
+            'destinationairport' => 'AGP',
+            'departuredate' => '20160930',
+            'adults' => '1',
+            'children' => '1',
+            'infants' => '1',
+        );
      }
 
+     /*
+     * MANDATORYPARAMS 
+     * 
+     * {('locale' => 'nl_BE')}
+     *
+     *
+     * OPTIONALSPARAMS 
+     * 
+     * {('departureairport' => 'OST'),('destinationairport' => 'ALC')}
+     *
+     */
      public function getFlightRoutes($params = null){
+
+        $params = $this->getParams($params, array('locale'));
+
         $client = new Client();
 
         $client->setAuth($this->user, $this->password, Client::AUTH_BASIC);
@@ -41,13 +73,29 @@ use Zend\Http\Request;
 
      }
 
-     public function getFlightSchedules($params = null){
+     /*
+     * MANDATORYPARAMS 
+     * 
+     * {('locale' => 'nl_BE'),('departureairport' => 'OST'),('destinationairport' => 'ALC')}
+     *
+     *
+     * OPTIONALSPARAMS 
+     * 
+     * {('returndepartureairport' => 'OST'),('returndestiantionairport' => 'ALC')}
+     *
+     */
+     public function getFlightSchedules($params = array()){
+
+        $params = $this->getParams($params, array('locale', 'departureairport', 'destinationairport'));
+
         $client = new Client();
 
         $client->setAuth($this->user, $this->password, Client::AUTH_BASIC);
         $client->setUri('http://dezemerel.ddns.net/api/json/1F/flightschedules/');
         $client->setMethod(Request::METHOD_GET);
-        $client->setParameterGet($params);
+        if($params){
+            $client->setParameterGet($params);
+        }
 
         $response = $client->send();
 
@@ -63,7 +111,21 @@ use Zend\Http\Request;
 
      }
 
+     /*
+     * MANDATORYPARAMS 
+     * 
+     * {('locale' => 'nl_BE'),('departureairport' => 'OST'),('destinationairport' => 'ALC'), ('departuredate' => '20160401'), ('adults' => '1'), ('children' => '1'), ('infants' => '1')}
+     *
+     *
+     * OPTIONALSPARAMS 
+     * 
+     * {('returndepartureairport' => 'OST'),('returndestiantionairport' => 'ALC'), ('returndate' => '20160401')}
+     *
+     */
      public function getFlightAvailability($params = null){
+
+        $params = $this->getParams($params, array('locale', 'departureairport', 'destinationairport', 'departuredate', 'adults', 'children', 'infants'));
+
         $client = new Client();
 
         $client->setAuth($this->user, $this->password, Client::AUTH_BASIC);
@@ -83,5 +145,18 @@ use Zend\Http\Request;
 
         return $flightavailability;
 
+     }
+
+     private function getParams($params, $mandatorykeys = null){
+
+            if($mandatorykeys){
+                foreach ($mandatorykeys as $mandatorykey) {
+                    if(!isset($params[$mandatorykey])){
+                        $params[$mandatorykey] = $this->mandatoryParams[$mandatorykey];
+                    }
+                }
+            }
+
+            return $params;
      }
  }
